9929c9f3efaf160762d88b18df2e63783d8cc25e
alina <sapron4ik93@gmail.com>
2022-10-25 14:19:22 -0500

    done

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import video from "./video.mp4"
+
+function App() {
+  const [activity,setActivity] = useState("")
+
+  const getActivity = async()=> {
+    const response = await fetch(`http://www.boredapi.com/api/activity/`)
+    const result = await response.json()
+    setActivity(result.activity)
+  }
+
+  useEffect(()=>{
+    getActivity()
+  },[])
+
+  return (
+    <div className="App">
+       <video autoPlay muted loop>
+            <source src={video} type="video/mp4"/>
+          </video>
+          <h1>Are you bored and have nothing to do? Let us advise!</h1>
+          <button onClick={getActivity}> Get new activity!</button>
+          <h2>{activity}</h2>
+          
+    </div>
+  );
+}
+
+export default App;
